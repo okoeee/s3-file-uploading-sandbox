f3eabd8dca765de70e3247ca336df2b72736befb
okoeee <yk27.puxcm@gmail.com>
2025-03-15 12:06:13 +0900

    update main exec

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,11 +1,29 @@
+import java.nio.file.Paths
+
 object Main extends App {
 
-  val credentialOpt = sys.env.get("MY_ENV_VAR")
+  val s3Client = S3ClientProvider.s3Client
 
-  for {
-    credential <- credentialOpt
-  } yield {
-    println(s"Credential: $credential")
+  // S3 bucket and object key
+  private val bucketName = "s3-file-uploading-sandbox"
+  private val objectKey = "1/batch-download/sample.txt"
+
+  // filePath
+  private val resource = getClass.getResource("/sample.txt")
+  private val filePath = Paths.get(resource.toURI)
+
+  S3ClientUploader
+    .uploadFile(
+      s3Client = s3Client,
+      bucketName = bucketName,
+      objectKey = objectKey,
+      filePath = filePath
+    ) match {
+    case util.Success(_)         => println("File uploaded successfully")
+    case util.Failure(exception) => println(s"File uploading failed: ${exception.getMessage}")
   }
 
+  s3Client.close()
+
+  println("File uploaded successfully\n")
 }
