dd31a9188ea3f0771804de1d67dd08525437daf4
okoeee <yk27.puxcm@gmail.com>
2025-03-21 23:29:37 +0900

    create simple lambda function

---
+++ src/main/scala/lambda/FileUploadHandler.scala
@@ -0,0 +1,16 @@
+package lambda
+
+import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
+import com.amazonaws.services.lambda.runtime.events.SNSEvent
+
+class FileUploadHandler extends RequestHandler[SNSEvent, String] {
+
+  override def handleRequest(snsEvent: SNSEvent, context: Context): String = {
+    println("Received SNS event: " + snsEvent)
+
+    val message = snsEvent.getRecords.get(0).getSNS.getMessage
+    println("From SNS: " + message)
+    message
+  }
+
+}
